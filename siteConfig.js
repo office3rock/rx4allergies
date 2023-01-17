df8295cad899dd142000e5ee2f461dee54202d78
office3rock <105702924+office3rock@users.noreply.github.com>
2023-01-17 13:48:53 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteUrl: "https://utnupes.com", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://rx4allergies.com", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "utnupes", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "rx4allergies", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "utnupes", // This allows an alternative site description for meta data for pages.
+    "rx4allergies", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "utnupes", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "rx4allergies", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: false,
-    title: "utnupes",
+    title: "rx4allergies",
     helpText: "הישארו מעודכנים",
     successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
@@ -25,11 +25,11 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://utnupes.com/",
+        url: "https://rx4allergies.com/",
       },
       {
         label: "יצירת קשר",
-        url: "https://utnupes.com/contact",
+        url: "https://rx4allergies.com/contact",
       },
     ],
   },
@@ -38,16 +38,16 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://utnupes.com/",
+        url: "https://rx4allergies.com/",
       },
       {
         label: "Sitemap",
-        url: "https://utnupes.com/sitemap.xml",
+        url: "https://rx4allergies.com/sitemap.xml",
       },
       
       {
         label: "יצירת קשר",
-        url: "https://utnupes.com/contact",
+        url: "https://rx4allergies.com/contact",
       },
       
     ],
@@ -70,7 +70,7 @@ module.exports = {
     successMessage: "We’ll get in touch with you soon.",
   },
   metadata: {
-    title: "utnupes",
+    title: "rx4allergies",
     description:
       "utnupes",
   },
@@ -82,15 +82,15 @@ module.exports = {
     username: "@DraftboxHQ",
   },
   facebookCard: {
-    title: "utnupes",
+    title: "rx4allergies",
     description:
       "Lightning fast, 1t on Gatsby.",
     imageUrl: "facebookImage.png",
     appId: "2391725224459953",
   },
-  siteTitle: "utnupes",
+  siteTitle: "rx4allergies",
   siteDescription:
-    "utnupes",
+    "rx4allergies",
   language: "he",
   logoUrl: "",
   iconUrl: "",
